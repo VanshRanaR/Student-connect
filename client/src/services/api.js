aaa67b60af76406584b50278431ca5234b9c2da9
Vansh Rana <vanshrana96669@gmail.com>
2026-02-18 20:03:24 +0530

    Added Render backend URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,8 @@
+// services/api.js
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:
-    window.location.hostname === "localhost"
-      ? "http://localhost:5000/api"
-      : "/api"
+  baseURL: "https://student-connect-l0or.onrender.com/api"
 });
 
 export default API;
