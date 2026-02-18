097298ec8c38eab1aa9e021acf38fd5f968b7387
Vansh Rana <vanshrana96669@gmail.com>
2026-02-18 19:55:48 +0530

    change in port

--- server/server.js
+++ server/server.js
@@ -83,7 +83,8 @@ app.use("/api/meetings", meetingRoutes);
 
 
 // Server start
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 server.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
